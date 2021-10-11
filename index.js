1ffaf20bb1b23a0fccbe6565342a9886d4bb3013
LancelotLewis <lancelot_lewis@163.com>
2021-10-11 17:38:38 +0800

    test upload

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const shell = require('shelljs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const endpoint = core.getInput('endpoint');
+  const accessToken = core.getInput('access-token');
+  // shell.exec(`blocklet config set registry ${endpoint}`);
+  shell.exec(`echo ${endpoint}`);
+
+  if (accessToken) {
+    // shell.exec(`blocklet upload --access-token ${accessToken}`);
+    shell.exec(`echo ${accessToken}`);
+  } else {
+    const developerSk = core.getInput('developer-sk');
+    // shell.exec(`blocklet publish --developer-sk ${developerSk}`);
+    shell.exec(`echo ${developerSk}`);
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
