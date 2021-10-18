7040b5d5688227adc3180dd1ce48b749f52fd264
LancelotLewis <lancelot_lewis@163.com>
2021-10-18 20:08:41 +0800

    v0.7.1

--- practice/command.js
+++ practice/command.js
@@ -3,15 +3,21 @@ const shell = require('shelljs');
 
 module.exports = async () => {
   try {
-    core
-      .getInput('command')
-      .split('\n')
-      .forEach((command) => {
-        shell.exec(command);
-        shell.pwd();
-        shell.ls();
-        shell.exec('process.cwd()');
-      });
+    shell.exec('pwd');
+    shell.cd('dist');
+    shell.exec('pwd');
+    shell.exec('ls');
+    const file = path.join(process.cwd(), 'index.js');
+    console.log('file-path', file);
+    // core
+    //   .getInput('command')
+    //   .split('\n')
+    //   .forEach((command) => {
+    //     shell.exec(command);
+    //     shell.exec('pwd');
+    //     shell.exec('ls');
+    //     shell.exec('process.cwd()');
+    //   });
   } catch (error) {
     core.setFailed(error.message);
   }
