cb162c0b89d2847497738f55aaa6ff91f8f24e11
LancelotLewis <lancelot_lewis@163.com>
2021-10-18 20:17:42 +0800

    v0.7.5

---
+++ dist/index1.js
@@ -0,0 +1,5 @@
+// const practiceExec = require('./practice/exec');
+// const practiceJumpPath = require('./practice/jump-path');
+const practiceCommand = require('./practice/command');
+
+practiceCommand();
--- practice/command.js
+++ practice/command.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const core = require('@actions/core');
 const shell = require('shelljs');
 
